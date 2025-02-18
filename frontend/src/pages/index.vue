e0c5db7b6743e75b34bd2062d2b1b552cd3062d3
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-02-18 21:31:00 +0300

    init commit

---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="w-full h-screen flex flex-col items-center justify-center">
+    <section class="w-xl h-64 rounded-lg bg-purple-100">
+      
+    </section>
+  </main>
+</template>
