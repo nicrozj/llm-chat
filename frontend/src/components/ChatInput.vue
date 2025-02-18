01a75e4fb57e3372d330c8dfd9f1b03cbaaed942
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-02-18 22:46:31 +0300

    added auto resize chat input

--- frontend/src/components/ChatInput.vue
+++ frontend/src/components/ChatInput.vue
@@ -1,6 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+let textarea = ref<HTMLTextAreaElement | null>(null);
+
+const resize = () => {
+  if (!textarea.value) return;
+
+  textarea.value.style.height = "";
+  textarea.value.style.height = `${Math.min(
+    textarea.value.scrollHeight + 2,
+    115
+  )}px`;
+};
+</script>
+
 <template>
   <div class="rounded-3xl ring w-xl bg-secondly ring-border p-2">
-    <textarea class="p-2 focus:outline-0 resize-none" placeholder="Сообщение" />
+    <textarea
+      @input="resize"
+      ref="textarea"
+      class="p-2 focus:outline-0 resize-none w-full"
+      placeholder="Сообщение"
+    />
     <div class="flex gap-4 justify-between">
       <div class="flex gap-4">
         <button
