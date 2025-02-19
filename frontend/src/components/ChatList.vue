7340abf629ce0bcabbdbaf387007720da518b96a
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-02-19 11:56:52 +0300

    added chat list

---
+++ frontend/src/components/ChatList.vue
@@ -0,0 +1,3 @@
+<template>
+  <section class="h-screen border-r-[1px] border-border w-xs">&nbsp;</section>
+</template>
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import Chat from "@/components/Chat.vue";
 import ChatInput from "@/components/ChatInput.vue";
+import ChatList from "@/components/ChatList.vue";
+import VStack from "@/components/stacks/VStack.vue";
 </script>
 
 <template>
-  <main class="w-full h-screen flex flex-col items-center justify-center">
-    <Chat></Chat>
-    <ChatInput />
+  <main class="w-full max-h-screen flex">
+    <ChatList />
+    <VStack class="w-full items-center">
+      <Chat class="grow" />
+      <ChatInput class="justify-self-end" />
+    </VStack>
   </main>
 </template>
