e2a391a6caa956e8e662218d0ef3fcae16c33238
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-02-25 11:48:09 +0300

    change loading spinner

--- frontend/src/components/LoadingSpinner.vue
+++ frontend/src/components/LoadingSpinner.vue
@@ -1,79 +1,19 @@
-<script setup lang="ts">
-import { defineProps } from "vue";
-
-interface Props {
-  loading?: boolean;
-  color?: string;
-  width?: number | string;
-  height?: number | string;
-}
-
-const props = withDefaults(defineProps<Props>(), {
-  loading: true,
-  color: "#333",
-  width: 50,
-  height: 40,
-});
-</script>
-
 <template>
-  <div v-if="loading" class="flex justify-center items-center">
-    <div
-      class="spinner"
-      :style="{
-        width: `${width}px`,
-        height: `${height}px`,
-      }"
-    >
+  <div v-if="loading" class="inline-flex">
+    <div class="w-5 h-5 relative">
       <div
-        v-for="index in 5"
-        :key="index"
-        class="rect"
-        :class="`rect${index}`"
-        :style="{ backgroundColor: color }"
+        class="absolute w-full h-full border-2 border-blue-500 border-t-transparent rounded-full animate-spin"
       ></div>
     </div>
   </div>
 </template>
 
-<style scoped>
-.spinner {
-  margin: 100px auto;
-  text-align: center;
-  font-size: 10px;
+<script setup lang="ts">
+interface Props {
+  loading?: boolean;
 }
 
-.rect {
-  height: 100%;
-  width: 6px;
-  display: inline-block;
-  animation: sk-stretchdelay 1.2s infinite ease-in-out;
-}
-
-.rect2 {
-  animation-delay: -1.1s;
-}
-
-.rect3 {
-  animation-delay: -1s;
-}
-
-.rect4 {
-  animation-delay: -0.9s;
-}
-
-.rect5 {
-  animation-delay: -0.8s;
-}
-
-@keyframes sk-stretchdelay {
-  0%,
-  40%,
-  100% {
-    transform: scaleY(0.4);
-  }
-  20% {
-    transform: scaleY(1);
-  }
-}
-</style>
+const props = withDefaults(defineProps<Props>(), {
+  loading: true,
+});
+</script>
